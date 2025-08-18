316151d17019ade9a8fa6be504065d171327289b
XenonAdonis <225632441+XenonAdonis@users.noreply.github.com>
2025-08-18 21:26:54 +0000

    ci: build web to web_dist [skip ci]

--- web_dist/flutter_service_worker.js
+++ web_dist/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "37d9fe589de1ec68b7aa9e6799d8539b",
+const RESOURCES = {"flutter_bootstrap.js": "f51741ea6917f23edd7b19b71317d897",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
